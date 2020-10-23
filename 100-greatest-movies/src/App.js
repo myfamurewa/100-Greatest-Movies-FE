f72fdcf7b983d088c321e8e423d36d5b9dd6235d
Michael Famurewa <myfamurewa@gmail.com>
2020-10-23 16:28:07 -0400

    Chakra is ok, but I need more practice with it

--- 100-greatest-movies/src/App.js
+++ 100-greatest-movies/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import './App.css';
-import PrivateRoute from "./components/PrivateRoute"
-import Home from './components/Home'
-import Movie from './components/Movie'
-import {Route, Switch} from "react-router-dom"
+import React from "react";
+import "./App.css";
+import PrivateRoute from "./components/PrivateRoute";
+import { theme, ThemeProvider } from "@chakra-ui/core";
+import Home from "./components/Home";
+import Movie from "./components/Movie";
+import { Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-   <Switch>
-   {/* <Route exact path="/" component={Login} /> */}
-   {/* <Route exact path="/sign-up" component={SignUp} /> */}
-   <Route path="/home" component={Home} />
-   <Route path="/movies/:id" component={Movie}/>
-   </Switch>
- </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Switch>
+          {/* <Route exact path="/login" component={Login} /> */}
+          {/* <Route exact path="/sign-up" component={SignUp} /> */}
+          <Route path="/" component={Home} />
+          <Route path="/movies/:id" component={Movie} />
+        </Switch>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- 100-greatest-movies/src/components/Home.js
+++ 100-greatest-movies/src/components/Home.js
@@ -1,45 +1,107 @@
-import React, {useState, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
-export default function Home() {
-    const [movies, setMovies] = useState([])
-    const [error, setError] = useState()
-    const [ratings, setRatings] = useState([])
-    useEffect(() => {
-       axios.get("http://localhost:5000/movies/").then(res => {
-           console.log(res)
-           setMovies(res.data)
-       }).catch(err => {
-           console.log(err)
-           setError(err)
-       })
+import React, { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Stack,
+  Heading,
+  Text,
+  Link,
+  Icon
+} from "@chakra-ui/core";
+import axios from "axios";
+export default function Home({ ThemeProvider }) {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState();
+  const [ratings, setRatings] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/movies/")
+      .then((res) => {
+        console.log(res);
+        setMovies(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+      });
 
-       axios.get("http://localhost:5000/ratings/").then(res => {
-           console.log("ratings", res)
-           setRatings(res.data)
-       }).catch(err => {
-           console.log("ratings error", err)
-       })
-    }, []) 
-    return ( 
-        <div>
-            {error && <span>An error has occurred {error}</span>}
-            {movies.map(movie => (
-                 <div className="movies" key={movie.id}>
-                    <Link to={`/movies/${movie.id}`} ><h1>{movie.id}. {movie.name}</h1></Link>
-                    <h4>Year: {movie.year}</h4>
-                    <ul>
-                    {movie.categories.split(",").map(category => (
-                        <li>{category}</li>
-                    ))}
-                    </ul>
-                    <p>Runtime: {movie.runtime}</p>
-                    <p>Summary: {movie.summary}</p>
-                    <p>Rating: {Math.round(ratings.filter(rating => rating.movie_id === movie.id).reduce((acc, cv) => {
-                        return acc + cv.rating
-                    }, 0) / ratings.filter(rating => rating.movie_id === movie.id).length)}⭐'s</p>
-                    </div>
+    axios
+      .get("http://localhost:5000/ratings/")
+      .then((res) => {
+        console.log("ratings", res);
+        setRatings(res.data);
+      })
+      .catch((err) => {
+        console.log("ratings error", err);
+      });
+  }, []);
+  return (
+    <Stack maxWidth="70vw" spacing={8} align="center" direction="row" shouldWrapChildren="true" >
+      {error && <span>An error has occurred {error}</span>}
+      {movies.map((movie) => (
+        <Box
+          p={5}
+          maxW="sm"
+          shadow="md"
+          borderWidth="1px"
+          flex="1"
+          round="md"
+          overflow="hidden"
+          className="movies"
+          p={4}
+          key={movie.id}
+        >
+          <Link color="gold" as={RouterLink} to={`/movies/${movie.id}`}>
+            <Heading as="h1" size="2x1">
+              {movie.id}. {movie.name}
+            </Heading>
+          </Link>
+          <Heading as="sm" size="3x2">{movie.year}</Heading>
+          <Box alignItems="center">
+          <List as="ul">
+            {movie.categories.split(",").map((category) => (
+              <ListItem display="inline">
+                <ListIcon>{category}</ListIcon>
+              </ListItem>
             ))}
-        </div>
-    )
+          </List>
+          </Box>
+          <Box
+            mt="1"
+            fontWeight="semibold"
+            as="h4"
+            lineHeight="tight"
+            isTruncated
+          >
+            <p>{movie.runtime} minutes</p>
+            <p>{movie.summary}</p>
+            <p>
+              Rating:{" "}
+              {Array(5)
+                .fill("")
+                .map((_, i) => (
+                    <Icon 
+                    name="star"
+                    color={i < Math.round(
+                        ratings
+                          .filter((rating) => rating.movie_id === movie.id)
+                          .reduce((acc, cv) => {
+                            return acc + cv.rating;
+                          }, 0) /
+                          ratings.filter((rating) => rating.movie_id === movie.id)
+                            .length
+                      ) ? "gold" : "gray.300"}>
+
+                    </Icon>
+                ))
+              }
+            </p>
+          </Box>
+        </Box>
+      ))}
+    </Stack>
+  );
 }
